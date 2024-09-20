61e8eaa26cb87c398065848d3c221189e0796b90
GioMeli <136624687+GioMeli@users.noreply.github.com>
2024-09-20 23:34:29 +0300

    Update UserDetailsServiceImpl.java

--- src /service/UserDetailsServiceImpl.java
+++ src /service/UserDetailsServiceImpl.java
@@ -1,1 +1,23 @@
+package gr.hua.agricoop.service;
+
+import gr.hua.agricoop.entity.Product;
+import gr.hua.agricoop.repository.ProductRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductDetailsServiceImpl {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Transactional
+    public ProductDetailsImpl loadProductById(Integer productId) {
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new IllegalArgumentException("Product Not Found with id: " + productId));
+
+        return ProductDetailsImpl.build(product);
+    }
+}
 

e5326dd95f66d31e1cad4c9522cdfc67ec513f0f
GioMeli <136624687+GioMeli@users.noreply.github.com>
2024-09-20 23:26:27 +0300

    Update UserRepository.java

--- src /repository/UserRepository.java
+++ src /repository/UserRepository.java
@@ -1,1 +1,17 @@
+package gr.hua.agricoop.repository;
+
+import gr.hua.agricoop.entity.Order;
+import io.swagger.v3.oas.annotations.Hidden;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@Hidden
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    Optional<Order> findByOrderNumber(String orderNumber);
+    Boolean existsByOrderNumber(String orderNumber);
+}
 

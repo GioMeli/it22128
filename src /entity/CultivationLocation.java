22522f4c316708f363d41210e767554a0b07e192
GioMeli <136624687+GioMeli@users.noreply.github.com>
2024-09-20 23:18:12 +0300

    Update CultivationLocation.java

--- src /entity/CultivationLocation.java
+++ src /entity/CultivationLocation.java
@@ -1,1 +1,105 @@
 
+package gr.hua.eventmanager.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.List;
+
+@Entity
+@Table(name = "event_venues")
+public class EventVenue {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Integer id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String location;
+
+    @Column
+    private String capacity;
+
+    @JsonIgnore
+    @ManyToMany(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH}, mappedBy = "venues")
+    private List<Event> events;
+
+    public EventVenue() {
+    }
+
+    public EventVenue(String name, String location, String capacity) {
+        this.name = name;
+        this.location = location;
+        this.capacity = capacity;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(String capacity) {
+        this.capacity = capacity;
+    }
+
+    public List<Event> getEvents() {
+        return events;
+    }
+
+    public void setEvents(List<Event> events) {
+        this.events = events;
+    }
+
+    public void addEvent(Event event) {
+        events.add(event);
+    }
+
+    public void removeEvent(Event event) {
+        events.remove(event);
+    }
+
+    @PreRemove
+    private void preRemove() {
+        for (Event event : events) {
+            event.removeVenue(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "EventVenue{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", location='" + location + '\'' +
+                ", capacity='" + capacity + '\'' +
+                '}';
+    }
+}

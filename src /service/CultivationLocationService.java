2cc87a41a09a7e90c5e7c42b679d7960dbdec3d7
GioMeli <136624687+GioMeli@users.noreply.github.com>
2024-09-20 23:32:20 +0300

    Update CultivationLocationService.java

--- src /service/CultivationLocationService.java
+++ src /service/CultivationLocationService.java
@@ -1,1 +1,57 @@
+package gr.hua.agricoop.service;
+
+import gr.hua.agricoop.entity.Product;
+import gr.hua.agricoop.repository.ProductRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Transactional
+    public List<Product> getProducts() {
+        return productRepository.findAll();
+    }
+
+    @Transactional
+    public Product getProduct(Integer productId) {
+        return productRepository.findById(productId).orElseThrow();
+    }
+
+    @Transactional
+    public Product saveProduct(Product product) {
+        productRepository.save(product);
+        return product;
+    }
+
+    @Transactional
+    public Product editProduct(Integer productId, Product updatedProduct) {
+        Product existingProduct = productRepository.findById(productId).orElse(null);
+        if (existingProduct != null) {
+            existingProduct.setName(updatedProduct.getName());
+            existingProduct.setPrice(updatedProduct.getPrice());
+            existingProduct.setDescription(updatedProduct.getDescription());
+            productRepository.save(existingProduct);
+        }
+        return existingProduct;
+    }
+
+    @Transactional
+    public void deleteProduct(Integer productId) {
+        productRepository.deleteById(productId);
+    }
+
+    @Transactional
+    public List<Product> getAvailableProducts() {
+        List<Product> products = productRepository.findAll();
+        products.removeIf(product -> !product.isAvailable());
+        return products;
+    }
+}
 

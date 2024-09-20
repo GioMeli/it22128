6c58df981cc2f6ccd7d56271a99ec6d74ad901b6
GioMeli <136624687+GioMeli@users.noreply.github.com>
2024-09-20 23:24:22 +0300

    Update CultivationLocationRepository.java

--- src /repository/CultivationLocationRepository.java
+++ src /repository/CultivationLocationRepository.java
@@ -1,1 +1,12 @@
 
+package gr.hua.agricoop.repository;
+
+import gr.hua.agricoop.entity.User;
+import io.swagger.v3.oas.annotations.Hidden;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Hidden
+public interface UserRepository extends JpaRepository<User, Long> {
+}

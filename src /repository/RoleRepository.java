01d5b4f302e45f3292d4a8b84fddf08afa7913d3
GioMeli <136624687+GioMeli@users.noreply.github.com>
2024-09-20 23:25:42 +0300

    Update RoleRepository.java

--- src /repository/RoleRepository.java
+++ src /repository/RoleRepository.java
@@ -1,1 +1,16 @@
 
+package gr.hua.serviceapp.repository;
+
+import gr.hua.serviceapp.entity.SubscriptionType;
+import io.swagger.v3.oas.annotations.Hidden;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@Hidden
+public interface SubscriptionTypeRepository extends JpaRepository<SubscriptionType, Integer> {
+
+    Optional<SubscriptionType> findByName(String name);
+}
